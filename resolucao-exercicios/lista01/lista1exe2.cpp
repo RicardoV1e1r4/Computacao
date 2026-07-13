be7ced8fd6b68720ac6b1f5f4756c4db8b1f494e
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-13 18:23:13 -0300

    Create lista1exe2.cpp
    Adição da resolução do exercício 2 da lista 1

---
+++ resolucao-exercicios/lista01/lista1exe2.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<locale.h>
+
+/*
+2) Faça uma função que receba como parâmetros as 2 notas de um aluno (valores reais), calcule e retorne a 
+média aritmética.
+*/
+
+float calculaMedia(float num1, float num2);
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	float num1, num2, mediaNota;
+	
+	printf("Calculadora de média das notas\n");
+	printf("\nDigite a primeira nota: ");
+	scanf("%f", &num1);
+	printf("\nDigite a primeira nota: ");
+	scanf("%f", &num2);
+	
+	mediaNota = calculaMedia(num1, num2);
+	
+	printf("\nA média das notas é %f", mediaNota);
+	
+	return 0;
+}
+
+float calculaMedia(float num1, float num2){
+	float media = (num1 + num2)/2;
+	
+	return media;
+}

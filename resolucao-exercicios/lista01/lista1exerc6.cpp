5062dff0030456c0d99cbe69039469b67fd4ab4d
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-15 18:01:06 -0300

    Resolução do exercício 6 da lista 1

--- resolucao-exercicios/lista01/lista1exerc6.cpp
+++ resolucao-exercicios/lista01/lista1exerc6.cpp
@@ -6,7 +6,27 @@ valor expresso em números de semanas e número de dias. Por exemplo, se o número 
 um evento for o valor 19, a função deve exibir: “2 semanas e 5 dias”
 */
 
+void qtd_semanas(int qtd_dias);
+
 int main(){
+	int dias;
+	
+	printf("Evento MUNDIAL");
+	printf("\nDigite a duracao do evento MUNDIAL em dias: ");
+	scanf("%d", &dias);
+	
+	qtd_semanas(dias);
 	
 	return 0;
 }
+
+void qtd_semanas(int qtd_dias){
+	int semanas, dias;
+	
+	semanas = qtd_dias/7;		// Quantidade de semanas - parte inteira da divisão
+	
+	dias = qtd_dias%7;			// Quantidade de dias - resto da divisão
+	
+	printf("\nA duracao do evento foi de %d semanas e %d dias", semanas, dias);
+}
+

738a14869c5625cd2b8a54f54912308eef630edf
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-14 22:41:35 -0300

    Resolução do exercício 6 da lista

---
+++ resolucao-exercicios/lista01/lista1exerc6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+6) Faça uma função que receba como parâmetro o número de dias decorrido em um evento e exiba o mesmo 
+valor expresso em números de semanas e número de dias. Por exemplo, se o número de dias decorrido em 
+um evento for o valor 19, a função deve exibir: “2 semanas e 5 dias”
+*/
+
+int main(){
+	
+	return 0;
+}

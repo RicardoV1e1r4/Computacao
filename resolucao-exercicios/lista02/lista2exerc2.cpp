e726a2387c98bd9e80963eb7cc8bebc2d8103658
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-16 09:09:39 -0300

    Create lista2exerc2.cpp
    Resolução do exercício 2 da lista 2

---
+++ resolucao-exercicios/lista02/lista2exerc2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/*
+2. Faça um programa que leia três números inteiros que serão fornecidos pelo usuário, calcule e exiba a soma 
+e a média dos números. Este programa deverá utilizar as seguintes funções: 
+a) calcula_soma: recebe como parâmetros os 3 números inteiros, calcula, exibe e retorna a soma. 
+b) calcula_media: recebe como parâmetro a soma dos números, calcula e exibe a média.
+*/
+
+int main(){
+	
+	return 0;
+}

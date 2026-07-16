6214f822b26114094afb83f03d3c9d35386a1c9b
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-15 22:48:01 -0300

    Resolução completa
    Resolução do exercício 1 da lista 2 completo

--- resolucao-exercicios/lista02/lista2exerc1.cpp
+++ resolucao-exercicios/lista02/lista2exerc1.cpp
@@ -1,9 +1,47 @@
 #include<stdio.h>
 
 /*
-
+1. Faça um programa que leia três números inteiros que serão fornecidos pelo usuário, calcule e exiba a soma 
+e a média dos números. Este programa deverá utilizar as seguintes funções: 
+a) calcula_soma: recebe como parâmetros os 3 números inteiros, calcula e retorna a soma. 
+b) calcula_media: recebe como parâmetros os 3 números inteiros, calcula e exibe a média
 */
 
+int calcula_soma(int num1, int num2, int num3);		// Item (a)
+
+void calcula_media(int num1, int num2, int num3);		// Item (b)
+
 int main(){
+	int n1, n2, n3;
+	
+	printf("\nDigite tres numeros para calcular a sua soma e media");
+	printf("\nDigite o primeiro numero:");
+	scanf("%d", &n1);
+	
+	printf("\nDigite o segundo numero:");
+	scanf("%d", &n2);
+	
+	printf("\nDigite o terceiro numero:");
+	scanf("%d", &n3);
+	
+	calcula_media(n1, n2, n3);
+	
 	return 0;
+}
+
+int calcula_soma(int num1, int num2, int num3){		// Item (a)
+	return num1 + num2 + num3;
+}
+
+void calcula_media(int num1, int num2, int num3){		// Item (b)
+	int soma;
+	float media;
+	
+	soma = calcula_soma(num1, num2, num3);
+	
+	media = soma/3;
+	
+	printf("\nSoma dos numeros: %d + %d + %d = %d", num1, num2, num3, soma);
+	
+	printf("\nMedia dos numeros: %d + %d + %d = %f", num1, num2, num3, media);
 }

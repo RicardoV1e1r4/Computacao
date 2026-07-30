8fac986027befdcde7ffb0bdd130df187b52578a
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-30 11:06:17 -0300

    Create lista6exerc3.cpp
    Inicio da resolução

---
+++ resolucao-exercicios/lista06/lista6exerc3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+3) Faça um programa, utilizando a função abaixo, que exiba o maior salário de cada departamento de uma 
+empresa e quantos funcionários ganham o maior salário do departamento. Para cada departamento, o 
+programa deverá ler o código do departamento e a quantidade de funcionários, e para cada funcionário, a 
+matrícula e o salário. Término da leitura dos departamentos: código do departamento = 0.
+a) Faça a função umDepartamento para processar os funcionários de um departamento. Esta função 
+deverá receber como parâmetro a quantidade de funcionários do departamento, ler os dados de cada 
+funcionário, descobrir o maior salário do departamento e quantos funcionários ganham este maior 
+salário, armazenando-os nas variáveis cujos endereços são fornecidos na chamada da função.
+*/
+
+int main(){
+	return 0;
+}
